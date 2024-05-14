0e99e7174ede31d55d848a87783dc0da4a599447
Ian Lum <ianlum314@gmail.com>
2024-05-14 16:23:46 -0700

    move around pipes

--- src/map/pipe.gd
+++ src/map/pipe.gd
@@ -1,9 +1,17 @@
 extends ColorRect
+class_name Pipe
 
 @onready var hovered_box = $Hovered
+
+signal clicked(pipe)
 
 func _on_mouse_entered():
 	hovered_box.show()
 
 func _on_mouse_exited():
 	hovered_box.hide()
+
+func _on_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			clicked.emit(self)
