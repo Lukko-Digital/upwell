ea05f251cc5c3ad99293b8950404abdb198a56d5
Miles <38819342+mrSquidee@users.noreply.github.com>
2024-06-06 14:39:45 -0700

    pipe attributes resource

---
+++ src/map/pipe_types/pipe_attributes.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name PipeAttributes
+
+@export var name: String = ""
+@export var resources = {"fuel": 0.0, "water": 0.0, "drill": 0.0}
+
+func get_info() -> String:
+    var info = "resources: "
+    for resource in resources:
+        if resources[resource] > 0:
+            info += "%.1f %s, " % [resources[resource],resource]
+    return info
