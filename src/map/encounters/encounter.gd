5e3e49d2717e9ea320c17798b1542219e01e0c59
Miles <38819342+mrSquidee@users.noreply.github.com>
2024-06-11 13:59:25 -0700

    removed old map

--- src/map/encounters/encounter.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name Encounter
-
-@export var prereq = {}
-@export var button_text = ""
-@export var enter_text = ""
-@export var cost = {}
-@export var reward = {}
-@export var next_encounters: Array[Encounter] = []
