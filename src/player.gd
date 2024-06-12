49da4b4e5bea0e4066011420301e26d98f405f1f
Ian Lum <ianlum314@gmail.com>
2024-06-12 15:00:22 -0700

    launch drill

--- src/player.gd
+++ src/player.gd
@@ -2,9 +2,13 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
+const DRILL = {
+	LAUNCH_SPEED = 600.0
+}
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@onready var drill_scene = preload ("res://src/drill_bit.tscn")
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var drill = null
 
 func _physics_process(delta):
 	handle_gravity(delta)
@@ -22,6 +26,15 @@ func handle_movement():
 func jump():
 	velocity.y = JUMP_VELOCITY
 
+func shoot():
+	var instance: RigidBody2D = drill_scene.instantiate()
+	instance.position = global_position
+	instance.set_axis_velocity((get_global_mouse_position() - global_position).normalized() * DRILL.LAUNCH_SPEED)
+	get_parent().add_child(instance)
+	drill = instance
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
 		jump()
+	elif event.is_action_pressed("shoot"):
+		shoot()
