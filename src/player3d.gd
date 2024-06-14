04b12c85c58056983a78e86077e3746b1884f808
Ian Lum <ianlum314@gmail.com>
2024-06-14 14:42:10 -0700

    refactor and tweak numbers

--- src/player3d.gd
+++ src/player3d.gd
@@ -1,21 +1,30 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const PLAYER = {
+	SPEED = 5.0,
+	JUMP_VELOCITY = 12.0
+}
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction = Input.get_axis("left", "right")
-	velocity.x = direction * SPEED
-
+	handle_gravity(delta)
+	handle_movement(delta)
 	move_and_slide()
+
+func handle_gravity(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta * 2
+
+func handle_movement(_delta):
+	var direction = Input.get_axis("left", "right")
+	velocity.x = direction * PLAYER.SPEED
+
+func jump():
+	if is_on_floor():
+		velocity.y = PLAYER.JUMP_VELOCITY
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		jump()
