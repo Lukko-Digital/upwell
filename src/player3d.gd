877c7379f9924c9751e215fe263c7386d9255ac1
Ian Lum <ianlum314@gmail.com>
2024-06-14 15:07:39 -0700

    launch balls

--- src/player3d.gd
+++ src/player3d.gd
@@ -5,8 +5,15 @@ const PLAYER = {
 	JUMP_VELOCITY = 12.0
 }
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+const DRILL = {
+	LAUNCH_SPEED = 20.0,
+}
+
+@onready var drill_scene = preload ("res://src/drill_bit.tscn")
+@onready var camera: Camera3D = $Camera3D
+
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var drill: RigidBody3D = null
 
 func _physics_process(delta):
 	handle_gravity(delta)
@@ -25,6 +32,16 @@ func jump():
 	if is_on_floor():
 		velocity.y = PLAYER.JUMP_VELOCITY
 
+func shoot():
+	var instance: RigidBody3D = drill_scene.instantiate()
+	instance.position = global_position + Vector3(0, 1, 0)
+	var vec_to_mouse = (camera.unproject_position(global_position) - get_viewport().get_mouse_position()).normalized()
+	instance.apply_central_impulse(Vector3( - vec_to_mouse.x, vec_to_mouse.y, 0) * DRILL.LAUNCH_SPEED)
+	get_parent().add_child(instance)
+	drill = instance
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
 		jump()
+	if event.is_action_pressed("repel"):
+		shoot()
