ba183a2faad33e57d502ef2ed6991b8b04c453a8
Ian Lum <ianlum314@gmail.com>
2024-06-16 16:17:19 -0700

    draw ag center

--- src/artificial_gravity.gd
+++ src/artificial_gravity.gd
@@ -2,4 +2,5 @@ extends Area2D
 class_name ArtificialGravity
 
 func _draw() -> void:
+    draw_circle(Vector2.ZERO, 4, Color.GREEN)
     draw_arc(Vector2.ZERO, $CollisionShape2D.shape.radius, 0, TAU, 20, Color.GREEN, 2)
