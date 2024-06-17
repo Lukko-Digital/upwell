fa275876151f77fae55d431b0e109b15b2f25340
Ian Lum <ianlum314@gmail.com>
2024-06-16 18:01:42 -0700

    fix classname issue

--- src/level_elements/artificial_gravity.gd
+++ src/level_elements/artificial_gravity.gd
@@ -1,6 +1,6 @@
 extends Area2D
-class_name Interactable
+class_name ArtificialGravity
 
 func _draw() -> void:
-    draw_circle(Vector2.ZERO, 4, Color.GREEN)
-    draw_arc(Vector2.ZERO, $CollisionShape2D.shape.radius, 0, TAU, 20, Color.GREEN, 2)
+	draw_circle(Vector2.ZERO, 4, Color.GREEN)
+	draw_arc(Vector2.ZERO, $CollisionShape2D.shape.radius, 0, TAU, 20, Color.GREEN, 2)
