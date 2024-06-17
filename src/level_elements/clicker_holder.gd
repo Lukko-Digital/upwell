7bdc72eac31867fe383c8f2538ce8adb3db5ffbd
Ian Lum <ianlum314@gmail.com>
2024-06-16 20:31:29 -0700

    pass clicker back and forth

--- src/level_elements/clicker_holder.gd
+++ src/level_elements/clicker_holder.gd
@@ -1,2 +1,11 @@
 extends Interactable
 class_name ClickerHolder
+
+var has_clicker: bool = true:
+	set(value):
+		$Clicker.visible = value
+		has_clicker = value
+
+func interact(player: Player):
+	player.has_clicker = has_clicker
+	has_clicker = !has_clicker
