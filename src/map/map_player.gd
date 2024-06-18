84eeba9d550394ab1a21771b43c67cd2856debb6
Ian Lum <ianlum314@gmail.com>
2024-06-18 14:54:09 -0700

    draw line and show motion

--- src/map/map_player.gd
+++ src/map/map_player.gd
@@ -3,16 +3,36 @@ class_name MapPlayer
 
 @onready var line: Line2D = $Line2D
 
-# func _ready() -> void:
-# 	print(line.get_point_count())
+var moving = false
+var destination: MapLocation
+
+func _process(_delta: float) -> void:
+	if moving:
+		line.set_point_position(1, destination.global_position - global_position)
 
 func location_hovered(location: MapLocation):
+	if moving:
+		return
 	if line.get_point_count() < 2:
-		line.add_point(location.position)
+		line.add_point(location.global_position - global_position)
 
-func location_unhovered(location: MapLocation):
+func location_unhovered(_location: MapLocation):
+	if moving:
+		return
 	if line.get_point_count() > 1:
 		line.remove_point(1)
 
 func location_selected(location: MapLocation):
-	pass
+	if moving:
+		return
+	destination = location
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "global_position", location.global_position, 1)
+	moving = true
+	await tween.finished
+	location_reached()
+
+func location_reached():
+	moving = false
+	if line.get_point_count() > 1:
+		line.remove_point(1)
