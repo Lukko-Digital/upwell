1cf4f29a7af55bacd7f25ac603362b3962f6c240
Ian Lum <ianlum314@gmail.com>
2024-06-18 19:49:11 -0700

    save level unlocks globally

--- src/level_elements/clicker_holder.gd
+++ src/level_elements/clicker_holder.gd
@@ -5,6 +5,7 @@ class_name ClickerHolder
 	set(value):
 		$Clicker.visible = value
 		has_clicker = value
+@export var unlocks_level: String
 
 @onready var id: String = owner.name + name
 
@@ -15,10 +16,19 @@ func _ready():
 	else:
 		# Load from Global state
 		has_clicker = Global.clicker_state[id]
+	
+	if unlocks_level and unlocks_level not in Global.level_unlocks:
+		Global.level_unlocks[unlocks_level] = false
 
 func interact(player: Player):
 	if has_clicker != player.has_clicker:
+		# exchange clicker with player
 		player.has_clicker = has_clicker
 		has_clicker = !has_clicker
+		# save global clicker state and level unlocks
 		Global.clicker_state[id] = has_clicker
+		if has_clicker and unlocks_level:
+			Global.level_unlocks[unlocks_level] = true
+			print(Global.level_unlocks)
+		# enable all AGs
 		get_tree().call_group("AGs", "enable")
