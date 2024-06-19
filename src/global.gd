e004714b05da097f303b22126400d690f423e282
Ian Lum <ianlum314@gmail.com>
2024-06-18 20:15:30 -0700

    map levels react to unlocking

--- src/global.gd
+++ src/global.gd
@@ -3,9 +3,8 @@ extends Node
 var clicker_state = {}
 var level_unlocks = {}
 
-signal level_unlocked(level)
+signal level_unlocked(level_name)
 
 func unlock_level(level_name: String):
     level_unlocks[level_name] = true
     level_unlocked.emit(level_name)
-    print(level_name)
--- src/map/map_location.gd
+++ src/map/map_location.gd
@@ -2,8 +2,14 @@ extends Area2D
 class_name MapLocation
 
 @export var level: PackedScene
+@export var locked_level_name: String
 
 @onready var player: MapPlayer = owner.get_node("MapPlayer")
+
+func _ready() -> void:
+	Global.level_unlocked.connect(level_unlocked)
+	if locked_level_name:
+		hide()
 
 func _on_mouse_entered() -> void:
 	player.location_hovered(self)
@@ -15,3 +21,7 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> voi
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			player.location_selected(self)
+
+func level_unlocked(level_name: String):
+	if level_name == locked_level_name:
+		show()
