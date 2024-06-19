59a233f21e17a3e394efb45840599007ac84de03
Ian Lum <ianlum314@gmail.com>
2024-06-18 19:32:51 -0700

    clicker state is saved

--- src/level_elements/clicker_holder.gd
+++ src/level_elements/clicker_holder.gd
@@ -6,8 +6,19 @@ class_name ClickerHolder
 		$Clicker.visible = value
 		has_clicker = value
 
+@onready var id: String = owner.name + name
+
+func _ready():
+	if id not in Global.clicker_state:
+		# Add state to Global state
+		Global.clicker_state[id] = has_clicker
+	else:
+		# Load from Global state
+		has_clicker = Global.clicker_state[id]
+
 func interact(player: Player):
 	if has_clicker != player.has_clicker:
 		player.has_clicker = has_clicker
 		has_clicker = !has_clicker
+		Global.clicker_state[id] = has_clicker
 		get_tree().call_group("AGs", "enable")
