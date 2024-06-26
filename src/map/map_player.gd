f68732c9091c77e2351885d90ccfbcdb227a74fd
Miles <38819342+mrSquidee@users.noreply.github.com>
2024-06-26 15:05:20 -0700

    map level rename and collision based level changing

--- src/map/map_player.gd
+++ src/map/map_player.gd
@@ -4,42 +4,32 @@ class_name MapPlayer
 @onready var line: Line2D = $Line2D
 
 var moving = false
-var destination: MapLocation
-var game: Game
-
-func _ready() -> void:
-	var current_scene = get_tree().get_current_scene()
-	if current_scene is Game:
-		game = current_scene
+var destination: MapLevel
 
 func _process(_delta: float) -> void:
 	if moving:
 		line.set_point_position(1, destination.global_position - global_position)
 
-func location_hovered(location: MapLocation):
+func location_hovered(location: MapLevel):
 	if moving:
 		return
 	if line.get_point_count() < 2:
 		line.add_point(location.global_position - global_position)
 
-func location_unhovered(_location: MapLocation):
+func location_unhovered(_location: MapLevel):
 	if moving:
 		return
 	if line.get_point_count() > 1:
 		line.remove_point(1)
 
-func location_selected(location: MapLocation):
+func location_selected(location: MapLevel):
 	if moving:
 		return
 	destination = location
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "global_position", location.global_position, 1)
 	moving = true
-	# await tween.finished
-	# location_reached()
-
-func location_reached(location: MapLocation):
+	await tween.finished
 	moving = false
 	if line.get_point_count() > 1:
 		line.remove_point(1)
-	game.change_level(location.level)
