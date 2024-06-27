4349f7f49981e95f31d1184c2ffdf1457733f547
Ian Lum <ianlum314@gmail.com>
2024-06-27 13:20:44 -0700

    add clicker movement, bouncing and ag interaction

--- src/level_elements/clicker/clicker_body.gd
+++ src/level_elements/clicker/clicker_body.gd
@@ -1,2 +1,14 @@
 extends GravitizedBody
 class_name ClickerBody
+
+const MAX_FALL_SPEED = 2600
+const BOUNCE_AMOUNT = 0.3
+
+func _physics_process(delta: float) -> void:
+	var gravity_state: GravityState = handle_artificial_gravity(delta)
+	handle_world_gravity(delta, gravity_state, MAX_FALL_SPEED)
+	handle_nudge(gravity_state)
+
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal()) * BOUNCE_AMOUNT
