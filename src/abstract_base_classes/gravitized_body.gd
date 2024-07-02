3a6a4a553cb12b5d1d013522117d7e2027d24888
Ian Lum <ianlum314@gmail.com>
2024-07-01 20:02:08 -0700

    lerp orbits

--- src/abstract_base_classes/gravitized_body.gd
+++ src/abstract_base_classes/gravitized_body.gd
@@ -10,6 +10,7 @@ const ARTIFICIAL_GRAVITY = {
 	PUSHPULL_SPEED = 3000.0,
 	ACCELERATION = 4.0, # lerp acceleration, unitless
 	ORBIT_SPEED = 800.0,
+	ORBIT_ANGLE = 70.0,
 	BOOST_VELOCITY = 3000.0,
 	NUDGE_DISTANCE = 50.0,
 	NUDGE_ACCELERATION = 0.1, # lerp acceleration, unitless
@@ -101,11 +102,14 @@ func handle_artificial_gravity(delta) -> GravityState:
 			var active_direction = Vector2.ZERO
 			if attracting:
 				# Right click, clockwise
-				active_direction = vec_to_gravity.orthogonal().normalized()
+				active_direction = vec_to_gravity.rotated( - deg_to_rad(ARTIFICIAL_GRAVITY.ORBIT_ANGLE)).normalized()
 			if repelling:
 				# Left click, counterclockwise
-				active_direction = -vec_to_gravity.orthogonal().normalized()
-			velocity = active_direction * ARTIFICIAL_GRAVITY.ORBIT_SPEED * speed_coef
+				active_direction = vec_to_gravity.rotated(deg_to_rad(ARTIFICIAL_GRAVITY.ORBIT_ANGLE)).normalized()
+			velocity = velocity.lerp(
+				active_direction * ARTIFICIAL_GRAVITY.ORBIT_SPEED * speed_coef,
+				ARTIFICIAL_GRAVITY.ACCELERATION * delta
+			)
 			return GravityState.ORBIT
 	return GravityState.NONE
 
