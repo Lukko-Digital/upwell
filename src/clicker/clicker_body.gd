5b9543e29029073df0f5f48c0334f6d2cb6fa807
Ian Lum <ianlum314@gmail.com>
2024-07-03 12:25:16 -0700

    scuffed clicker bounce-roll-slide implementation

--- src/clicker/clicker_body.gd
+++ src/clicker/clicker_body.gd
@@ -6,16 +6,32 @@ const BOUNCE_AMOUNT = 0.3
 
 ## Set false when dropped to prevent immediately re-entering holder
 var catchable = true
+var frames_since_last_collision = 0
 
 func _physics_process(delta: float) -> void:
 	var gravity_state: GravityState = handle_artificial_gravity(delta)
 	handle_world_gravity(delta, gravity_state, MAX_FALL_SPEED)
 	handle_nudge(gravity_state)
+	# Apply normal force and friction
+	var floor_normal = get_floor_normal()
+	if floor_normal != Vector2.UP:
+		# On slope, roll
+		velocity.x += (floor_normal * world_gravity * delta * 0.8).x
+	else:
+		# On flat ground, friction
+		velocity.x = move_toward(velocity.x, 0, delta * 500)
 
-	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
-	if collision:
-		catchable = true
-		velocity = velocity.bounce(collision.get_normal()) * BOUNCE_AMOUNT
+	var prev_vel = velocity
+	var collided = move_and_slide()
+
+	if collided and frames_since_last_collision > 5:
+		var collision = get_last_slide_collision()
+		velocity = prev_vel.bounce(collision.get_normal()) * BOUNCE_AMOUNT
+
+	if collided:
+		frames_since_last_collision = 0
+	else:
+		frames_since_last_collision += 1
 
 func handle_artificial_gravity(delta: float):
 	if !Global.player_has_clicker:
