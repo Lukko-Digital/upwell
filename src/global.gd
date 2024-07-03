3a29e35269b10a5a90909bb3fa57fcfe2a96f3cc
Joshua Ehrlich <joshuatehrlich@gmail.com>
2024-07-02 17:23:33 -0700

    change level titles in globals

--- src/global.gd
+++ src/global.gd
@@ -3,14 +3,22 @@ extends Node
 ## NULL is not a level but allows receivers to indicate that they do not unlock
 ## any level
 enum LevelIDs {
-    NULL,
-    DAD,
-    L0,
-    L0A,
-    L1,
-    L2,
-    L3,
-    WIN,
+	NULL,
+	L1A,
+	L1B,
+	L1BB,
+	L1C,
+	L2A,
+	L2B,
+	L2C,
+	L3A,
+	L3B,
+	L3BB,
+	L3C,
+	L4A,
+	L4B,
+	L4C,
+	START
 }
 
 var player_has_clicker = false
@@ -21,5 +29,5 @@ var npc_conversation_state = {}
 signal level_unlocked(level_name: LevelIDs)
 
 func unlock_level(level_name: LevelIDs):
-    level_unlocks[level_name] = true
-    level_unlocked.emit(level_name)
+	level_unlocks[level_name] = true
+	level_unlocked.emit(level_name)
