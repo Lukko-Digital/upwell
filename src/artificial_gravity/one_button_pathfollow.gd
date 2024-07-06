e54d8809015473afe8d20931bc437956938bcf9d
Ian Lum <ianlum314@gmail.com>
2024-07-06 12:43:43 -0700

    create parent pathfollow class, calculates its own velocity

--- src/artificial_gravity/one_button_pathfollow.gd
+++ src/artificial_gravity/one_button_pathfollow.gd
@@ -1,21 +1,14 @@
-extends PathFollow2D
+extends ButtonControlledPathFollow
 class_name OneButtonPathFollow
 
-@export var speed: float = 400
 @export var button: HolderButton
-@export var line: Line2D
 
 ## +1 or -1, representing forward or backwards
 var direction = 1
 
-func _ready() -> void:
-    loop = false
-    for point in get_parent().curve.get_baked_points():
-        line.add_point(point)
-
-func _process(delta: float) -> void:
-    if button.has_clicker:
-        progress += speed * delta * direction
-    match progress_ratio:
-        0.0: direction = 1
-        1.0: direction = -1
+func handle_movement(delta):
+	if button.has_clicker:
+		progress += speed * delta * direction
+	match progress_ratio:
+		0.0: direction = 1
+		1.0: direction = -1
