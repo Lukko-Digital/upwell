1468fa07f4710efc9aa3b6966870971688833450
Miles <38819342+mrSquidee@users.noreply.github.com>
2024-07-10 13:44:08 -0700

    screen interactable scene

--- src/map/screen_interactable.gd
+++ src/map/screen_interactable.gd
@@ -1,5 +1,11 @@
 extends Interactable
 class_name ScreenInteractable
 
+@onready var subviewport: SubViewport = $SubViewportContainer/SubViewport
+@export var screen: PackedScene
+
+func _ready() -> void:
+	subviewport.add_child(screen.instantiate())
+
 func interact(_player: Player):
 	Global.set_camera_focus.emit(global_position)
