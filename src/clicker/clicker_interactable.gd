758075e24b2301de04b391b8f227984c0d3c6fb9
Ian Lum <ianlum314@gmail.com>
2024-07-10 15:16:37 -0700

    clicker interact label stays right side up

--- src/clicker/clicker_interactable.gd
+++ src/clicker/clicker_interactable.gd
@@ -3,6 +3,9 @@ class_name ClickerInteractable
 
 @onready var body = get_parent()
 
+func _process(_delta: float) -> void:
+	rotation = -body.rotation
+
 func interact(player: Player):
 	player.add_clicker(body)
 
