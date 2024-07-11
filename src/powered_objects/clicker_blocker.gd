5e1829268624d848741834c56609dd05df7e94bf
Ian Lum <ianlum314@gmail.com>
2024-07-10 19:07:55 -0700

    powered blocker

--- src/powered_objects/clicker_blocker.gd
+++ src/powered_objects/clicker_blocker.gd
@@ -11,6 +11,14 @@ func _ready() -> void:
 	sprite.scale.x = $CollisionShape2D.shape.radius / DEFAULT_WIDTH
 	sprite.scale.y = $CollisionShape2D.shape.height / DEFAULT_HEIGHT
 
+func enable():
+	set_deferred("monitoring", true)
+	sprite.frame = 0
+
+func disable():
+	set_deferred("monitoring", false)
+	sprite.frame = 1
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is ClickerBody:
 		body.return_to_home()
