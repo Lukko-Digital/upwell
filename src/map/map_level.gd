9a0485fd4a7871387a0834fcc32448dcb7b190f9
Joshua Ehrlich <joshuatehrlich@gmail.com>
2024-07-14 14:26:51 -0700

    make visited places gray instd of geren

--- src/map/map_level.gd
+++ src/map/map_level.gd
@@ -45,6 +45,6 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> voi
 
 func _on_area_entered(area: Area2D):
 	if area.get_name() == "PlayerBody":
-		modulate = Color.GREEN
+		modulate = Color.DIM_GRAY
 		player.destination = self
 		game.change_level(level)
