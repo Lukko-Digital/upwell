95329929cabf8ee31dd3d5fb390cd5ce8a70ce99
Ian Lum <ianlum314@gmail.com>
2024-07-13 22:37:08 -0700

    delete clicker info resource i never actually used this idk why it got added skull emoji

--- src/clicker/clicker_info.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-## Used by the player to remember what clickers to respawn, as clickers are
-## despawned when entering the players inventory
-class_name ClickerInfo
-
-var home_holder: ClickerHolder
-
-func _init(home_holder_: ClickerHolder) -> void:
-    home_holder = home_holder_
