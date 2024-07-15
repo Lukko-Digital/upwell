ed46691b36e46c3ec66f336c544eaf58dd411d4c
Joshua Ehrlich <joshuatehrlich@gmail.com>
2024-07-14 21:07:40 -0700

    global dialoguye conditions

--- src/global.gd
+++ src/global.gd
@@ -1,11 +1,39 @@
 extends Node
 
 var dialogue_conditions = {
-	"LOC_RELAPSE": true,
-	"GREETED_RELAPSE": false,
-	"GREETED": false,
+	
+	#GENERAL
+	"HAS_CLICKER": false,
+	
+	#DAD
+	"DAD_MET": false,
+	
+	#OLD MAN
+	"OLD_MET_RELAPSE": false,
+	"OLD_MET": false,
 	"OLD_KNOWS_TERIN": false,
-	"RLQ1": true
+	"OLD_RLQ1": true,
+	
+	#LOCATIONS
+	"AT_H32_NUCLEAR": false,
+	"AT_001_CONTACT": false,
+	"AT_002_FALL": false,
+	"AT_003_TEST": false,
+	"AT_004_RELAPSE": false,
+	"AT_005_STORAGE": false,
+	"AT_H02_RECURRENCE": false,
+	"AT_UNKNOWN": false,
+	
+	"BEEN_H32_NUCLEAR": false,
+	"BEEN_001_CONTACT": false,
+	"BEEN_002_FALL": false,
+	"BEEN_003_TEST": false,
+	"BEEN_004_RELAPSE": false,
+	"BEEN_005_STORAGE": false,
+	"BEEN_H02_RECURRENCE": false,
+	"BEEN_UNKNOWN": false,
+	
+	"LEFT_HOME": false
 }
 
 var pod_has_clicker = false
@@ -24,3 +52,7 @@ func call_pod(empty_pod: EmptyPod):
 	pod_position = empty_pod
 	pod_position.handle_empty()
 	pod_called.emit(empty_pod)
+
+func _process(delta):
+	if (dialogue_conditions["BEEN_001_CONTACT"] or dialogue_conditions["BEEN_002_FALL"] or dialogue_conditions["BEEN_003_TEST"] or dialogue_conditions["BEEN_004_RELAPSE"] or dialogue_conditions["BEEN_005_STORAGE"] or dialogue_conditions["BEEN_H02_RECURRENCE"] or dialogue_conditions["BEEN_UNKNOWN"]):
+		dialogue_conditions["LEFT_HOME"] = true
