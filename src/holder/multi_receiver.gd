251b7a30a509eafde3f2df435cc4c1107d469b88
Ian Lum <ianlum314@gmail.com>
2024-07-14 17:58:02 -0700

    multi receiver can start completed

--- src/holder/multi_receiver.gd
+++ src/holder/multi_receiver.gd
@@ -5,22 +5,30 @@ class_name MultiReceiver
 
 signal completion_state_changed(completed: bool)
 
-var completed: bool = false:
+var completed: bool:
     set(value):
         completion_state_changed.emit(value)
         completed = value
 
 func _ready() -> void:
+    if all_holders_have_clickers():
+        on_completion()
+    else:
+        completed = false
+    
     for receiver in receivers:
         receiver.clicker_state_changed.connect(_receiver_state_changed)
 
 func on_completion():
     completed = true
 
+func all_holders_have_clickers():
+    return receivers.all(func(rec): return rec.has_clicker())
+
 func _receiver_state_changed(_receiver: ClickerHolder, has_clicker: bool):
     if has_clicker:
         # Clicker inserted
-        if receivers.all(func(rec): return rec.has_clicker()):
+        if all_holders_have_clickers():
             # All clickers are in
             on_completion()
     else:
