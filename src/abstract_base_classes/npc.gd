cb572ff3c6e3bb306b49414a56b0a45792b8f755
Joshua Ehrlich <joshuatehrlich@gmail.com>
2024-07-14 23:04:21 -0700

    add npc name export

--- src/abstract_base_classes/npc.gd
+++ src/abstract_base_classes/npc.gd
@@ -3,11 +3,13 @@ class_name NPC
 
 @export_file("*.csv") var dialogue_file
 
+@export var npc_name: String
+
 var conversation_tree: ConversationTree
 
 func _ready() -> void:
 	super()
-	conversation_tree = DialogueParser.parse_csv(dialogue_file, name)
+	conversation_tree = DialogueParser.parse_csv(dialogue_file, "[center]"+npc_name+"[/center]")
 
 func interact(player: Player):
 	player.start_dialogue(self)
