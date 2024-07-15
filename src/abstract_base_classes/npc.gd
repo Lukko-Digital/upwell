d345c514986f4acdf90ed427aa81e97245935f96
Ian Lum <ianlum314@gmail.com>
2024-07-14 17:13:34 -0700

    remove print

--- src/abstract_base_classes/npc.gd
+++ src/abstract_base_classes/npc.gd
@@ -8,7 +8,6 @@ var conversation_tree: ConversationTree
 func _ready() -> void:
 	super()
 	conversation_tree = DialogueParser.parse_csv(dialogue_file, name)
-	conversation_tree.print()
 
 func interact(player: Player):
 	player.start_dialogue(self)
