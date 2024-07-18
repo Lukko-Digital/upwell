9cb38dbcf9012c304e4f31e0b7fe2868fa9b963d
Ian Lum <ianlum314@gmail.com>
2024-07-17 18:44:37 -0700

    pause text animation at punctuation

--- src/dialogue/dialogue_ui.gd
+++ src/dialogue/dialogue_ui.gd
@@ -2,6 +2,9 @@ extends CanvasLayer
 class_name DialogueUI
 
 const TEXT_SPEED = 0.03
+## Multiplier on how long it will take for the next character to appear
+const END_CHARACTER_SLOWDOWN = 15
+const COMMA_SLOWDOWN = 5
 
 @export var dialogue_label: RichTextLabel
 @export var name_label: RichTextLabel
@@ -69,10 +72,31 @@ func play_branch(branch_id: String):
 	animate_display()
 
 func animate_display():
+	## Just the characters that will be seen, no bbcode
+	var raw_text = BBCodeParser.strip_bbcode(dialogue_label.text)
 	dialogue_label.visible_characters = 0
-	while dialogue_label.visible_characters < len(dialogue_label.text):
+	while dialogue_label.visible_characters < raw_text.length():
+		var display_speed_coef = 1
 		dialogue_label.visible_characters += 1
-		display_timer.start(TEXT_SPEED)
+
+		## The character that was just revealed
+		var new_char = raw_text[dialogue_label.visible_characters - 1]
+		var next_char = ""
+		if dialogue_label.visible_characters < raw_text.length():
+			next_char = raw_text[dialogue_label.visible_characters]
+		match new_char:
+			".":
+				# Don't slow down "..." as much
+				if next_char == ".":
+					display_speed_coef = COMMA_SLOWDOWN
+				else:
+					display_speed_coef = END_CHARACTER_SLOWDOWN
+			"!", "?":
+				display_speed_coef = END_CHARACTER_SLOWDOWN
+			",":
+				display_speed_coef = COMMA_SLOWDOWN
+
+		display_timer.start(TEXT_SPEED * display_speed_coef)
 		await display_timer.timeout
 
 func spawn_reponse(response: Response, display_time: float):
