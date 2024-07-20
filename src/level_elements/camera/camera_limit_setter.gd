912f8eaabe59b8a176e7dff8ad649ad69de76cfe
Ian Lum <ianlum314@gmail.com>
2024-07-20 14:04:24 -0700

    powered limit settter

--- src/level_elements/camera/camera_limit_setter.gd
+++ src/level_elements/camera/camera_limit_setter.gd
@@ -5,3 +5,9 @@ class_name CameraLimitSetter
 func _ready() -> void:
     set_collision_mask_value(1, false)
     set_collision_layer_value(1, false)
+
+func set_disabled(value: bool):
+    for child in get_children():
+        if child is CollisionShape2D or child is CollisionPolygon2D:
+            child.disabled = value
+    print(name)
