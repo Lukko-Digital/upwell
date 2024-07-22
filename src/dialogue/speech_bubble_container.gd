664aaf2a3f63d6244411c6f1c1d30d8ced6daf71
Ian Lum <ianlum314@gmail.com>
2024-07-22 10:15:44 -0700

    switch nodule to sprite 2d

--- src/dialogue/speech_bubble_container.gd
+++ src/dialogue/speech_bubble_container.gd
@@ -1,6 +1,11 @@
 @tool
 extends MarginContainer
 
+## Somewhat arbitrary number, determined by just tweaking the value until speech
+## bubbble lines up with nodule, will need change as the scale of various
+## elements change
+const NODULE_OFFSET = -10
+
 func _process(_delta: float) -> void:
-	position.y = -size.y * scale.y + 18
+	position.y = -size.y * scale.y + NODULE_OFFSET
 	size.y = 0
