6d96c8fff02afea04736a551b217b7fcdaa40870
Ian Lum <ianlum314@gmail.com>
2024-07-22 11:58:45 -0700

    npc optionally faces you during dialogueg

--- src/abstract_base_classes/npc.gd
+++ src/abstract_base_classes/npc.gd
@@ -2,8 +2,10 @@ extends Interactable
 class_name NPC
 
 @export_file("*.csv") var dialogue_file
-
-@export var npc_name: String
+@export var faces_you_during_dialogue = true
+## If the NPC sprite is normally facing to the left, check this box
+@export var default_left_facing = false
+@export var npc_sprite: Sprite2D
 
 @onready var nodule: Sprite2D = $Nodule
 
@@ -11,10 +13,23 @@ var conversation_tree: ConversationTree
 
 func _ready() -> void:
 	super()
-	assert(nodule != null, "NPC \"" + name + "\" has no nodule")
 	nodule.hide()
 	conversation_tree = DialogueParser.parse_csv(dialogue_file)
 
 func interact(player: Player):
+	if faces_you_during_dialogue:
+		var vec_to_player = player.global_position - global_position
+		var player_on_right = vec_to_player.x > 0
+		## Truth table
+		## player is on the right | default left facing | flip
+		## -------------------------------------------
+		## true           		  | true                | true
+		## true           		  | false               | false
+		## false         		  | true                | false
+		## false          		  | false               | true
+		npc_sprite.flip_h = (player_on_right == default_left_facing)
+		nodule.position.x = sign(vec_to_player.x) * abs(nodule.position.x)
+		nodule.flip_h = player_on_right
+
 	player.start_dialogue(self)
 	Global.set_camera_focus.emit(self)
