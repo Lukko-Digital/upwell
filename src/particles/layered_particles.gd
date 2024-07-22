bbbbe86c7ff7de9db099673a214fe30ad3b1e8aa
Joshua Ehrlich <joshuatehrlich@gmail.com>
2024-07-21 23:06:15 -0700

    make layered particles color customizable

---
+++ src/particles/layered_particles.gd
@@ -0,0 +1,11 @@
+@tool
+extends Node2D
+
+func _process(_delta):
+	handle_particles()
+
+func handle_particles():
+	for child in get_children():
+		var target = child.get_child(0)
+		print(target)
+		target.modulate = modulate
