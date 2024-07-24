1516b94ae0673e502268c86723326e48e192ac5d
Joshua Ehrlich <joshuatehrlich@gmail.com>
2024-07-23 22:33:23 -0700

    adjust sizing and fonts

--- src/dialogue/speech_bubble_container.gd
+++ src/dialogue/speech_bubble_container.gd
@@ -4,7 +4,7 @@ extends MarginContainer
 ## Somewhat arbitrary number, determined by just tweaking the value until speech
 ## bubbble lines up with nodule, will need change as the scale of various
 ## elements change
-const NODULE_OFFSET = -10
+const NODULE_OFFSET = -4
 
 func _process(_delta: float) -> void:
 	position.y = -size.y * scale.y + NODULE_OFFSET
