4a264213deee379b7eadf63398f9049ee21fa6f5
Ian Lum <ianlum314@gmail.com>
2024-07-27 10:47:35 -0700

    make trajectory line collidable

--- src/screen_puzzles/screen_player.gd
+++ src/screen_puzzles/screen_player.gd
@@ -2,14 +2,21 @@
 extends Node2D
 class_name ScreenPlayer
 
-@export var STARTING_POWER: float = 1000
-const SPACING: float = 2
+# @export var STARTING_POWER: float = 1000
+# const SPACING: float = 2
+const STARTING_POWER: float = 200
+const SPACING: float = 10
 
 @onready var line: Line2D = $Line2D
+@onready var line_area: Area2D = $Line2D/LineCollisionArea
+
+func _ready() -> void:
+	update_tragectory()
 
 ## Returns the folder that was hit, if no folder was hit, returns null
 func update_tragectory() -> ScreenCore:
 	line.clear_points()
+	clear_collision_segments()
 
 	var dir: Vector2 = Vector2.UP
 	var in_ag: ScreenAG = null
@@ -18,7 +25,7 @@ func update_tragectory() -> ScreenCore:
 	var world_physics := get_world_2d().direct_space_state
 	var query := PhysicsPointQueryParameters2D.new()
 	query.collide_with_areas = true
-	query.collision_mask = 32
+	query.collision_mask = 32 # Layer 6
 	query.position = global_position
 
 	var power = STARTING_POWER
@@ -64,9 +71,27 @@ func update_tragectory() -> ScreenCore:
 				dir = -orth
 
 		line.add_point(query.position - global_position)
+		spawn_collision_segment()
 		query.position += dir * SPACING
 		power -= 1
 	return null
 
-func _process(_delta: float) -> void:
-	update_tragectory()
+func clear_collision_segments():
+	for child in line_area.get_children():
+		line_area.remove_child.call_deferred(child)
+		child.queue_free()
+
+func spawn_collision_segment():
+	var points = line.points
+	if points.size() < 2:
+		return
+	var collision = CollisionShape2D.new()
+	var segment = SegmentShape2D.new()
+	segment.a = points[- 1]
+	segment.b = points[- 2]
+	collision.shape = segment
+	line_area.add_child.call_deferred(collision)
+
+# func _process(_delta: float) -> void:
+# 	update_tragectory()
+	# print(line_area.get_child_count())
