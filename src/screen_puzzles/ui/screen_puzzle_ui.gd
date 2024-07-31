7dbb167fb08b0b72eee254589e23dacdcca0f589
Ian Lum <ianlum314@gmail.com>
2024-07-31 10:27:57 -0700

    screen puzzle text types out

--- src/screen_puzzles/ui/screen_puzzle_ui.gd
+++ src/screen_puzzles/ui/screen_puzzle_ui.gd
@@ -7,6 +7,7 @@ class_name ScreenPuzzleUI
 
 @onready var screen_player: ScreenPlayer = %ScreenPlayer
 
+const CHARS_PER_FRAME = 5
 const LAUNCH_TEXT = {
 	SUCCESS = "[wave amp=150.0 freq=5.0 connected=1][color=green][center] LAUNCH SUCCESS [/center][/color][/wave]",
 	FAIL = "[shake rate=20.0 level=30 connected=1][color=red][center] LAUNCH FAILED [/center][/color][/shake]",
@@ -18,14 +19,21 @@ func _ready() -> void:
 
 	launch_result_label.hide()
 
+func animate_display():
+	main_text_label.visible_characters = 0
+	while main_text_label.visible_characters < main_text_label.text.length():
+		main_text_label.visible_characters += CHARS_PER_FRAME
+		await get_tree().process_frame
+
 func _on_folder_opened(text: String):
 	main_text_label.text = text
+	animate_display()
 
 func launch_success(folder: ScreenCore):
 	folder.opened = true
 	folder.launch_success()
 	screen_player.launch_success()
-	
+
 	## REPLACE THIS WITH YOUR CODE
 	launch_result_label.text = LAUNCH_TEXT.SUCCESS
 	launch_result_label.show()
@@ -40,7 +48,6 @@ func launch_fail():
 	await get_tree().create_timer(1).timeout
 	launch_result_label.hide()
 	## ---
-
 
 func _on_launch_button_pressed() -> void:
 	var folder: ScreenCore = screen_player.targeted_folder
