7d4625a5fec8f41d4130b35124643134cef83dd8
Joshua Ehrlich <joshuatehrlich@gmail.com>
2024-08-03 14:43:43 -0700

    wip clean process TODO: move snapping to locations out of process to an area2d entered signal handle, you should snap even when youre holding shift

--- src/map/map_player/map_player.gd
+++ src/map/map_player/map_player.gd
@@ -54,37 +54,21 @@ func _process(delta: float) -> void:
 	if moving:
 		# Resolve gravitized state
 		var active_ag = grav_component.check_active_ag()
-		var gravity_state = grav_component.determine_gravity_state(active_ag)
-		if gravity_state != GravitizedComponent.GravityState.NONE and Global.pod_has_clicker:
-			manual_control = true
-			var new_vel = grav_component.calculate_gravitized_velocity(
-				active_ag, gravity_state, velocity, delta
-			)
-			velocity = new_vel.normalized() * SPEED
+		var gravity_state = handle_artificial_gravity(active_ag, delta)
 
-		# if you stop holding shift on a entry point, you snap there
-		elif gravity_state == GravitizedComponent.GravityState.NONE:
-			for area in collision_box.get_overlapping_areas():
-				if area is Entrypoint and destination == area:
-					destination = area
+
+		# # if you stop holding shift on a entry point, you snap there
+		# if gravity_state == GravitizedComponent.GravityState.NONE:
+		# 	for area in collision_box.get_overlapping_areas():
+		# 		if area is Entrypoint and destination == area:
+		# 			destination = area
 
 		global_position += velocity * delta
 
-		if manual_control:
-			location_deselected()
-			# shake
-			if gravity_state == GravitizedComponent.GravityState.BOOST:
-				Global.main_camera.set_shake_lerp(BOOST_SHAKE_AMOUNT, 2.0)
-			Global.main_camera.target_shake_amount = ORBIT_SHAKE_AMOUNT
-			# draw the line
-			line.set_point_position(1, velocity.normalized() * distance_per_energy)
-			if gravity_state == GravitizedComponent.GravityState.ORBIT:
-				boost_line.set_point_position(1, active_ag.global_position.direction_to(global_position) * distance_per_energy / 2)
-			else:
-				boost_line.set_point_position(1, Vector2.ZERO)
-		else:
-			line.set_point_position(1, (destination.global_position - global_position).limit_length(distance_per_energy))
-		
+		draw_destination_line(gravity_state)
+		draw_boost_line(gravity_state, active_ag)
+		handle_manual_control_shake(gravity_state)
+
 		if at_destination():
 			end_movement()
 
@@ -99,9 +83,25 @@ func _process(delta: float) -> void:
 
 ## ----------------------------- MOVING -----------------------------
 
-func at_destination() -> bool:
-	return global_position.distance_to(destination.global_position) < 20
+func handle_artificial_gravity(active_ag: ArtificialGravity, delta) -> GravitizedComponent.GravityState:
+	if not Global.pod_has_clicker:
+		return GravitizedComponent.GravityState.NONE
+		
+	var gravity_state = grav_component.determine_gravity_state(active_ag)
+	if gravity_state != GravitizedComponent.GravityState.NONE:
+		manual_control = true
+		var new_vel = grav_component.calculate_gravitized_velocity(
+			active_ag, gravity_state, velocity, delta
+		)
+		velocity = new_vel.normalized() * SPEED
+	return gravity_state
 
+## checks if we are within a threshold distance to the destination
+func at_destination() -> bool:
+	const THRESHOLD = 20
+	return global_position.distance_to(destination.global_position) < THRESHOLD
+
+## called once when you start travelling
 func travel() -> void:
 	if moving:
 		return
@@ -114,6 +114,7 @@ func travel() -> void:
 	Global.main_camera.start_shake()
 	Global.main_camera.set_shake_lerp(TRAVEL_SHAKE_AMOUNT, BASE_SHAKE_LERP_SPEED)
 
+## called when you recall and reach destination
 func end_movement() -> void:
 	moving = false
 	manual_control = false
@@ -140,6 +141,17 @@ func recall() -> void:
 	location_deselected()
 	end_movement()
 
+## ----------------------------- FUEL -----------------------------
+
+func enter_coolant_pocket() -> void:
+	energy_bar.value = energy_bar.max_value
+	in_coolant = true
+	map_animation_player.play("neutral")
+	game.pod.pod_animation_player.play("neutral")
+	
+func exit_coolant_pocket() -> void:
+	in_coolant = false
+
 ## ----------------------------- MAP INTERACTION -----------------------------
 
 func location_selected(location: Entrypoint):
@@ -165,11 +177,30 @@ func location_deselected():
 
 ## ------------------------- DRAW LINE -------------------------
 
+func draw_destination_line(gravity_state: GravitizedComponent.GravityState):
+	if gravity_state == GravitizedComponent.GravityState.ORBIT:
+		line.set_point_position(1, velocity.normalized() * distance_per_energy)
+	else:
+		line.set_point_position(1, (destination.global_position - global_position).limit_length(distance_per_energy))
+		
+func draw_boost_line(gravity_state: GravitizedComponent.GravityState, active_ag: ArtificialGravity):
+	if gravity_state == GravitizedComponent.GravityState.ORBIT:
+		boost_line.set_point_position(1, active_ag.global_position.direction_to(global_position) * distance_per_energy / 2)
+	else:
+		boost_line.set_point_position(1, Vector2.ZERO)
+
 func calculate_line_distance() -> float:
 	distance_per_energy = SPEED / (ENERGY_USE_RATE / energy_bar.max_value) * (energy_bar.value / energy_bar.max_value)
 	return distance_per_energy
 
 ## ------------------------- ANIMATION -------------------------
+
+func handle_manual_control_shake(gravity_state: GravitizedComponent.GravityState):
+	match gravity_state:
+		GravitizedComponent.GravityState.BOOST:
+			Global.main_camera.set_shake_lerp(BOOST_SHAKE_AMOUNT, 2.0)
+		GravitizedComponent.GravityState.ORBIT:	
+			Global.main_camera.target_shake_amount = ORBIT_SHAKE_AMOUNT
 
 func low_energy() -> void:
 	map_animation_player.play("LOW_ENERGY")
