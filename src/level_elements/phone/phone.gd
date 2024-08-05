14d8ca0b0953059550e99d1bed485e3a8cb8228a
Ian Lum <ianlum314@gmail.com>
2024-08-05 13:18:27 -0700

    fade in and out small phone sprite

--- src/level_elements/phone/phone.gd
+++ src/level_elements/phone/phone.gd
@@ -4,8 +4,9 @@ class_name Phone
 @onready var dial: Sprite2D = $RotatableDial
 @onready var buttons_container: Node2D = $PhoneNumberButtons
 @onready var lights_container: HBoxContainer = $Lights
-@onready var phone_sprite: AnimatedSprite2D = $Phone
-@onready var phone_glow: Sprite2D = $Phone/PhoneGlow
+@onready var receiver_sprite: AnimatedSprite2D = $Receiver
+@onready var receiver_glow: Sprite2D = $Receiver/ReceiverGlow
+@onready var small_phone_sprite: Sprite2D = $PhoneSmall
 @onready var npc_node: NPC = $PhoneNPC
 
 ## Set when player first interacts
@@ -18,8 +19,8 @@ var phone_number: Array[int] = []
 func _ready() -> void:
 	super()
 	# Init visuals
-	phone_sprite.play("in")
-	phone_glow.hide()
+	receiver_sprite.play("in")
+	receiver_glow.hide()
 	# Connect signals
 	for button: PhoneNumberButton in buttons_container.get_children():
 		button.clicked.connect(_on_button_clicked)
@@ -32,15 +33,19 @@ func interact(player_: Player):
 		player.active_phone = self
 		player.velocity = Vector2.ZERO
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		var tween = create_tween()
-		tween.tween_property(player, "modulate", Color(Color.WHITE, 0), 0.2)
+		tween_opacity(player, 0, 0.2)
+		tween_opacity(small_phone_sprite, 0, 0.2)
 	else:
 		Global.main_camera.set_focus(null)
 		focused = false
 		player.active_phone = null
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		var tween = create_tween()
-		tween.tween_property(player, "modulate", Color(Color.WHITE, 1), 0.2)
+		tween_opacity(player, 1, 0.2)
+		tween_opacity(small_phone_sprite, 1, 0.2)
+
+func tween_opacity(node: CanvasItem, opacity: float, time: float):
+	var tween = create_tween()
+	tween.tween_property(node, "modulate", Color(Color.WHITE, opacity), time)
 
 func tween_dial_rotation(angle: float, time: float, trans: Tween.TransitionType) -> Tween:
 	var tween = create_tween()
@@ -71,9 +76,9 @@ func reset():
 	phone_number.clear()
 	for light: PhoneLight in lights_container.get_children():
 		light.unglow()
-	phone_sprite.play("out")
+	receiver_sprite.play("out")
 	await get_tree().create_timer(0.2).timeout
-	phone_sprite.play("in")
+	receiver_sprite.play("in")
 
 ## Check if the dialed number has a corresponding Global dialogue variable.
 ## Returns the dictionary key for the dialed number if it exists, or the empty
@@ -109,7 +114,7 @@ func _on_phone_area_input_event(_viewport: Node, event: InputEvent, _shape_idx: 
 		phone_picked()
 
 func _on_phone_area_mouse_entered() -> void:
-	phone_glow.show()
+	receiver_glow.show()
 
 func _on_phone_area_mouse_exited() -> void:
-	phone_glow.hide()
+	receiver_glow.hide()
