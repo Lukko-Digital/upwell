ccb5d3055db299605dfc64fc8a4e22516f80e324
Joshua Ehrlich <joshuatehrlich@gmail.com>
2024-08-05 18:15:49 -0700

    make interact labels fade out/in when walking near interactable

--- src/abstract_base_classes/interactable.gd
+++ src/abstract_base_classes/interactable.gd
@@ -2,6 +2,7 @@ extends Area2D
 class_name Interactable
 
 @onready var interact_label: Label = $InteractLabel
+@onready var opacity_tween: Tween
 
 ## If `true`, this interactable is the one currently highlighted by the player
 ## for interaction
@@ -9,11 +10,14 @@ var highlighted: bool = false:
 	set = _set_highlighted
 
 func _set_highlighted(value: bool):
-		interact_label.visible = value
+		if opacity_tween: opacity_tween.kill()
+		opacity_tween = create_tween()
+		opacity_tween.tween_property(interact_label, "modulate", Color(Color.WHITE, float(value)), 0.2)
 		highlighted = value
 
 func _ready() -> void:
-	interact_label.hide()
+	interact_label.show()
+	interact_label.modulate = Color(Color.WHITE, 0)
 
 func interact(_player: Player):
 	pass
