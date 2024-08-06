c1c96bf9398d6a41dd56ca38b85916e8c473ab14
Ian Lum <ianlum314@gmail.com>
2024-08-06 11:58:48 -0700

    log reads from text file attached to map levels

--- src/map/map_level.gd
+++ src/map/map_level.gd
@@ -3,21 +3,24 @@ extends MapLocation
 class_name MapLevel
 
 @export var level: PackedScene
-
-@export var label: Label
-
+@export_file("*.txt") var log_file
 @export var name_text: String:
 	set(value):
 		name_text = value
-		label.text = value
-		
+		name_label.text = value
+@export var name_label: Label
+
 @onready var game: Game = get_tree().get_current_scene()
 @onready var level_sprite = get_sprite()
+
+var log_dict: Dictionary
 
 func _on_area_entered(_area: Area2D):
 	player.hit_hazard()
 
 func _ready() -> void:
+	if log_file:
+		log_dict = LogFileParser.parse(log_file)
 	if not Engine.is_editor_hint():
 		hide()
 
--- src/map/map_player/map_ui.gd
+++ src/map/map_player/map_ui.gd
@@ -1,2 +1,26 @@
 extends CanvasLayer
 class_name MapUI
+
+const UNDISCOVERED_TEXT = "[b]UNDISCOVERED[/b]\n\n"
+
+@onready var map_player: MapPlayer = get_parent()
+@onready var log_label: RichTextLabel = %LogLabel
+
+func _ready() -> void:
+    map_player.select_destination.connect(_on_select_destination)
+    Global.set_dialogue_variable("MET_FRAUD", false)
+    Global.set_dialogue_variable("VISITED_RUINS", false)
+    
+func _on_select_destination(location: Entrypoint):
+    if location == null:
+        return
+    var level: MapLevel = location.get_parent()
+    var text = ""
+    var num_undiscovered = 0
+    for key in level.log_dict:
+        if Global.dialogue_conditions[key]:
+            text += level.log_dict[key]
+        else:
+            num_undiscovered += 1
+    text += UNDISCOVERED_TEXT.repeat(num_undiscovered)
+    log_label.text = text
