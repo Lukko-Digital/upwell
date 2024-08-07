7d76ce914aca8d6b70a29d2d21ff7d08a108a14d
Ian Lum <ianlum314@gmail.com>
2024-08-07 13:14:57 -0700

    hovered cursor for phone

--- src/level_elements/phone/phone.gd
+++ src/level_elements/phone/phone.gd
@@ -3,15 +3,23 @@ class_name Phone
 
 const POD_CODE = [1, 1, 1, 1]
 
+# Dial
 @onready var dial: Sprite2D = $RotatableDial
 @onready var buttons_container: Node2D = $PhoneNumberButtons
 @onready var lights_container: HBoxContainer = $Lights
+# Receiver
 @onready var receiver_sprite: AnimatedSprite2D = $Receiver
+@onready var receiver_area: Area2D = $Receiver/ReceiverArea
 @onready var receiver_glow: Sprite2D = $Receiver/ReceiverGlow
+# Other
 @onready var small_phone_sprite: Sprite2D = $PhoneSmall
 @onready var npc_node: NPC = $PhoneNPC
 @onready var blur: ColorRect = $Blur
-@onready var blur_tween: Tween
+
+var blur_tween: Tween
+
+var default_cursor = load("res://assets/puzzle_elements/temp_mouse.png")
+var hovered_cursor = load("res://assets/puzzle_elements/temp_mouse2.png")
 
 ## Set when player first interacts
 var player: Player
@@ -20,6 +28,8 @@ var focused: bool = false
 var rotating: bool = false
 var phone_number: Array[int] = []
 
+## ------------------------------ CORE ------------------------------
+
 func _ready() -> void:
 	super()
 	# Init visuals
@@ -27,7 +37,12 @@ func _ready() -> void:
 	receiver_glow.hide()
 	# Connect signals
 	for button: PhoneNumberButton in buttons_container.get_children():
-		button.clicked.connect(_on_button_clicked)
+		button.clicked.connect(_on_phone_button_clicked)
+		button.mouse_entered.connect(_on_phone_button_mouse_entered)
+		button.mouse_exited.connect(_on_phone_button_mouse_exited)
+	receiver_area.input_event.connect(_on_receiver_area_input_event)
+	receiver_area.mouse_entered.connect(_on_receiver_area_mouse_entered)
+	receiver_area.mouse_exited.connect(_on_receiver_area_mouse_exited)
 
 func interact(player_: Player):
 	player = player_
@@ -60,6 +75,8 @@ func interact(player_: Player):
 		blur_tween = create_tween()
 		blur_tween.tween_property(blur.material, "shader_parameter/blur_amount", 0, 0.2)
 
+## ------------------------------ VISUALS ------------------------------
+
 func tween_opacity(node: CanvasItem, opacity: float, time: float):
 	var tween = create_tween()
 	tween.tween_property(node, "modulate", Color(Color.WHITE, opacity), time)
@@ -68,6 +85,8 @@ func tween_dial_rotation(angle: float, time: float, trans: Tween.TransitionType)
 	var tween = create_tween()
 	tween.tween_property(dial, "rotation", angle, time).set_trans(trans)
 	return tween
+
+## ------------------------------ PHONING ------------------------------
 
 func number_dialed(number: int):
 	phone_number.append(number)
@@ -119,7 +138,17 @@ func get_phone_number_key() -> String:
 	else:
 		return ""
 
-func _on_button_clicked(button: PhoneNumberButton):
+## ------------------------------ CURSOR ------------------------------
+
+func set_hovered_cursor():
+	Input.set_custom_mouse_cursor(hovered_cursor)
+
+func set_default_cursor():
+	Input.set_custom_mouse_cursor(default_cursor)
+
+## ------------------------------ SIGNALS ------------------------------
+
+func _on_phone_button_clicked(button: PhoneNumberButton):
 	if rotating or phone_number.size() >= 4:
 		return
 
@@ -131,7 +160,13 @@ func _on_button_clicked(button: PhoneNumberButton):
 	await tween_dial_rotation(0, angle * 1.5 / TAU, Tween.TRANS_LINEAR).finished
 	rotating = false
 
-func _on_phone_area_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+func _on_phone_button_mouse_entered():
+	set_hovered_cursor()
+
+func _on_phone_button_mouse_exited():
+	set_default_cursor()
+
+func _on_receiver_area_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if (
 		event is InputEventMouseButton and
 		event.button_index == MOUSE_BUTTON_LEFT and
@@ -139,8 +174,9 @@ func _on_phone_area_input_event(_viewport: Node, event: InputEvent, _shape_idx: 
 	):
 		phone_picked()
 
-func _on_phone_area_mouse_entered() -> void:
+func _on_receiver_area_mouse_entered() -> void:
 	receiver_glow.show()
+	set_hovered_cursor()
 
-func _on_phone_area_mouse_exited() -> void:
-	receiver_glow.hide()
+func _on_receiver_area_mouse_exited() -> void:
+	set_default_cursor()
