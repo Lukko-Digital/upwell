819f295edaee1dd4e552880995eee9f85c22b724
Ian Lum <ianlum314@gmail.com>
2024-08-06 19:15:16 -0700

    parallax canvas has parallax layer export that sets layer, viewport scale, and name

--- src/level_elements/parallax_canvas.gd
+++ src/level_elements/parallax_canvas.gd
@@ -3,6 +3,24 @@ extends CanvasLayer
 ## Moves all child nodes to be the same global position as parent node
 class_name ParallaxCanvas
 
+## Sets the [follow_viewport_scale] equal to the value and sets [layer] and
+## [name] according to the formula below. Value must be greater than 0.
+## Converting to canvas layer number:
+## When less than one, subtract 1 from the number and multiply by 100.
+## e.g. 0.99 = -1, 0.9 = -10, 0.5 = -50
+## When greater than one, multiply by 10. e.g. 1 = 10, 1.5 = 15, 2.1 = 21
+@export var parallax_layer: float:
+    set(value):
+        if value < 1:
+            layer = roundi(100.0 * (value - 1.0))
+        elif value >= 1:
+            layer = roundi(value * 10)
+        
+        follow_viewport_scale = value
+        name = str(layer).replace(".", "_")
+        parallax_layer = value
+        update_configuration_warnings()
+
 ## By default only moves children on ready, if true, continuously checks for
 ## movement n process
 @export var updates_in_process: bool = false
@@ -24,3 +42,11 @@ func move_children():
     last_parent_pos = parent.global_position
     for child in get_children():
         child.global_position = last_parent_pos
+
+func _get_configuration_warnings() -> PackedStringArray:
+    var warnings = []
+
+    if parallax_layer <= 0:
+        warnings.append("Parallax Layer must be greater than 0")
+
+    return warnings
