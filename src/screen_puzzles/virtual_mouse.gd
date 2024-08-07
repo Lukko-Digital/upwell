16d04c1dcdf79c5bfaec4f3b54dda3ac452bd1fa
Ian Lum <ianlum314@gmail.com>
2024-08-07 11:51:34 -0700

    virtual mouse hides itself based on global focus signal

--- src/screen_puzzles/virtual_mouse.gd
+++ src/screen_puzzles/virtual_mouse.gd
@@ -1,4 +1,13 @@
 extends Sprite2D
 
+func _ready() -> void:
+    Global.camera_focus_changed.connect(_on_camera_focus_changed)
+
 func _process(_delta):
     global_position = get_global_mouse_position() + Vector2(11, 11)
+
+func _on_camera_focus_changed(focus: Node2D):
+    if focus == null:
+        hide()
+    elif focus is ScreenInteractable:
+        show()
