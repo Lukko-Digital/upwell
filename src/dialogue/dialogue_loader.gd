3c17e6cfa28664057abe9a339b747019cf3ef2b5
Ian Lum <ianlum314@gmail.com>
2024-08-08 22:32:42 -0700

    only take over path when new

--- src/dialogue/dialogue_loader.gd
+++ src/dialogue/dialogue_loader.gd
@@ -5,7 +5,9 @@ class_name DialogueLoader
 @export_global_file("*.csv") var csv_file:
     set(value):
         csv_file = value.get_file()
-        take_over_path(resource_path.get_base_dir() + "/" + csv_file.get_basename() + ".tres")
+        var new_resource_path = resource_path.get_base_dir() + "/" + csv_file.get_basename() + ".tres"
+        if resource_path != new_resource_path:
+            take_over_path(new_resource_path)
 
 func load_csv() -> FileAccess:
     var file_path = resource_path.get_base_dir() + "/" + csv_file
